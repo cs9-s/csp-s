0f88dea2fe9a787f42554a8dd825b595b292e7b1
cs9-s <csp.s37@gmail.com>
2026-07-13 13:34:25 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Welcome to CSP-S");
+  console.log("Welcome to CSP-S");
 const cursor = document.querySelector(".cursor");
 
 
@@ -36,6 +36,81 @@ document.querySelectorAll("a, button").forEach(item => {
         cursor.style.height = "20px";
 
     });
+
+});
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 55,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    color: {
+      value: "#ff2a2a"
+    },
+    shape: {
+      type: "circle"
+    },
+    opacity: {
+      value: 0.4,
+      random: true
+    },
+    size: {
+      value: 2.5,
+      random: true
+    },
+    line_linked: {
+      enable: false
+    },
+    move: {
+      enable: true,
+      speed: 1.2,
+      direction: "none",
+      random: true,
+      straight: false,
+      out_mode: "out"
+    }
+  },
+  interactivity: {
+    detect_on: "window",
+    events: {
+      onhover: {
+        enable: false
+      },
+      onclick: {
+        enable: false
+      },
+      resize: true
+    }
+  },
+  retina_detect: true
+});
+document.addEventListener("DOMContentLoaded", () => {
+
+    const music = document.getElementById("bgMusic");
+    const button = document.getElementById("musicToggle");
+
+    button.onclick = function () {
+
+        if (music.paused) {
+
+            music.play();
+
+            button.classList.add("playing");
+            document.body.classList.add("music-playing");
+
+        } else {
+
+            music.pause();
+
+            button.classList.remove("playing");
+            document.body.classList.remove("music-playing");
+
+        }
+
+    };
 
 });
 
